0b0fb323afab7a20f752a8b03fb3464f6ffd0874
donovan-westwater <donovan.westwater@gmail.com>
2024-07-24 23:10:19 -0700

    Went back to testing model

--- VulkanPractice/main.cpp
+++ VulkanPractice/main.cpp
@@ -25,7 +25,7 @@ private:
     const int MAX_FRAMES_IN_FLIGHT = 2; //The amount of frames that can be processed concurrently
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
-    const std::string MODEL_PATH = "Models/CornellBox-Original.obj";//"Models/Guilmon.obj";
+    const std::string MODEL_PATH = "Models/CrappyCornellBox_TriVersion.obj";//"Models/Guilmon.obj";
     const std::string TEXTURE_PATH = "Textures/TestTex.png";
     const std::string MATERIALS_PATH = "Materials/";//"Materials/CrappyCornellBox_TriVersion.mtl";
 
