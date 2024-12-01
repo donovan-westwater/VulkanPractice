10d45a7b15b4abb226d417e8623a59ca3caf0eef
donovan-westwater <donovan.westwater@gmail.com>
2024-12-01 12:29:01 -0800

    Added the OpenUSD lib using relative pathing and putting the lib files in the project.

---
+++ VulkanPractice/Libraries/OpenUSD/lib/usd/usd/resources/codegenTemplates/wrapTokens.cpp
@@ -0,0 +1,55 @@
+//
+// Copyright 2016 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+// GENERATED FILE.  DO NOT EDIT.
+#include <boost/python/class.hpp>
+#include "{{ libraryPath }}/tokens.h"
+
+{% if useExportAPI %}
+{{ namespaceUsing }}
+
+{% endif %}
+namespace {
+
+// Helper to return a static token as a string.  We wrap tokens as Python
+// strings and for some reason simply wrapping the token using def_readonly
+// bypasses to-Python conversion, leading to the error that there's no
+// Python type for the C++ TfToken type.  So we wrap this functor instead.
+class _WrapStaticToken {
+public:
+    _WrapStaticToken(const TfToken* token) : _token(token) { }
+
+    std::string operator()() const
+    {
+        return _token->GetString();
+    }
+
+private:
+    const TfToken* _token;
+};
+
+template <typename T>
+void
+_AddToken(T& cls, const char* name, const TfToken& token)
+{
+    cls.add_static_property(name,
+                            boost::python::make_function(
+                                _WrapStaticToken(&token),
+                                boost::python::return_value_policy<
+                                    boost::python::return_by_value>(),
+                                boost::mpl::vector1<std::string>()));
+}
+
+} // anonymous
+
+void wrap{{ tokensPrefix }}Tokens()
+{
+    boost::python::class_<{{ tokensPrefix }}TokensType, boost::noncopyable>
+        cls("Tokens", boost::python::no_init);
+{% for token in tokens %}
+    _AddToken(cls, "{{ token.id }}", {{ tokensPrefix }}Tokens->{{ token.id }});
+{% endfor %}
+}
