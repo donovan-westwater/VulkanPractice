413ef1f0039347f9bdc8292c259b1d2df5de0598
donovan-westwater <donovan.westwater@gmail.com>
2024-12-09 23:58:32 -0800

    the python parts of boost cant find their source files. Going to look around OpenUSD bin and Plugin bin to see if there is something I missed

---
+++ VulkanPractice/LevelManager.cpp
@@ -0,0 +1,10 @@
+#include "common.h"
+#include "ResourceManager.h"
+#include <pxr/usd/usdGeom/xform.h>
+#include <pxr/usd/usd/stage.h>
+#include "LevelManager.h"
+
+void LevelManager::testImport() {
+	testPointer = UsdStage::Open("dino.obj");
+	stage->Export("dino.usd");
+}
---
+++ VulkanPractice/LevelManager.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef LEVEL_MANAGER_H
+#define LEVEL_MANAGER_H
+
+#include "common.h"
+#include "ResourceManager.h"
+#include <pxr/usd/usdGeom/xform.h>
+#include <pxr/usd/usd/stage.h>
+
+class LevelManager {
+public:
+	UsdStageRefPointer testPointer;
+
+	void testImport();
+};
+#endif // !LEVEL_MANAGER_H
