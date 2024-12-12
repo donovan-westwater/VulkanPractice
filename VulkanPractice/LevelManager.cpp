b96352a5df238096a1436692ec34787363674cd3
donovan-westwater <donovan.westwater@gmail.com>
2024-12-12 00:38:41 -0800

    I have been moving around include files trying in vain to get this stupid library to be recognized properly. It cant find the include files even though I have added them into the project so I am trying to figure out what is wrong. It is annoying and difficult

--- VulkanPractice/LevelManager.cpp
+++ VulkanPractice/LevelManager.cpp
@@ -1,7 +1,7 @@
-#include "common.h"
-#include "ResourceManager.h"
-#include <pxr/usd/usdGeom/xform.h>
-#include <pxr/usd/usd/stage.h>
+//#include "common.h"
+//#include "ResourceManager.h"
+//#include <pxr/usd/usdGeom/xform.h>
+//#include <pxr/usd/usd/stage.h>
 #include "LevelManager.h"
 
 void LevelManager::testImport() {
--- VulkanPractice/LevelManager.h
+++ VulkanPractice/LevelManager.h
@@ -4,7 +4,7 @@
 
 #include "common.h"
 #include "ResourceManager.h"
-#include <pxr/usd/usdGeom/xform.h>
+//#include <pxr/usd/usdGeom/xform.h>
 #include <pxr/usd/usd/stage.h>
 
 class LevelManager {
